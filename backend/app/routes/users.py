3cbca2608d12e55841378627ac5fa190bb75e6db
Anasp7 <anasabdulnazarp@gmail.com>
2025-09-18 23:53:36 +0530

    Update users.py

--- backend/app/routes/users.py
+++ backend/app/routes/users.py
@@ -1,1 +1,8 @@
+from flask import Blueprint, jsonify
+
+users_bp = Blueprint("users", __name__)
+
+@users_bp.route("/", methods=["GET"])
+def get_users():
+    return jsonify({"message": "Users route is working!"})
 
